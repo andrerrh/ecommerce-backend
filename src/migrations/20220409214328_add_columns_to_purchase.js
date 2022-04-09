6de233073daeacbe1aa95a8fa680179d7369c8bc
andrerrh <andre.rodrigues.he@gmail.com>
2022-04-09 18:44:45 -0300

    Added two columns to purchase table

---
+++ src/migrations/20220409214328_add_columns_to_purchase.js
@@ -0,0 +1,18 @@
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.up = function(knex) {
+    return knex.schema.alterTable('purchases', table => {
+        table.string('product_name',1000).notNullable()
+        table.string('product_image',1000).notNullable()
+    })
+};
+
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.down = function(knex) {
+  
+};
