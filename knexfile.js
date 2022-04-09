3ddc9ec335a4fce61870371b0d2500629e49923d
andrerrh <andre.rodrigues.he@gmail.com>
2022-04-09 13:05:57 -0300

    Changed password settings

--- knexfile.js
+++ knexfile.js
@@ -1,4 +1,3 @@
-const { password } = require('./.env')
 /**
  * @type { Object.<string, import("knex").Knex.Config> }
  */
@@ -8,7 +7,8 @@ module.exports = {
     host: 'ec2-3-230-122-20.compute-1.amazonaws.com',
     database: 'd3bn5eri2pjkfq',
     user: 'wxgrtlknkroaxs',
-    password
+    port: 5432,
+    password: '208110fe7c016c4727518c22bdb83ebf17bdec9c72da9c1c711d653608d3ece1'
   },
   pool: {
     min: 2,
