b8f0b853b4d185a502e8cc7aa44a0c8ff0cef866
andrerrh <andre.rodrigues.he@gmail.com>
2022-04-09 13:46:56 -0300

    Changed connection settings

--- knexfile.js
+++ knexfile.js
@@ -8,13 +8,17 @@ module.exports = {
     database: 'd3bn5eri2pjkfq',
     user: 'wxgrtlknkroaxs',
     port: 5432,
-    password: '208110fe7c016c4727518c22bdb83ebf17bdec9c72da9c1c711d653608d3ece1'
+    password: '208110fe7c016c4727518c22bdb83ebf17bdec9c72da9c1c711d653608d3ece1',
+    ssl: {
+      require: true,
+      rejectUnauthorized: false
+    }
   },
-  pool: {
-    min: 2,
-    max: 10
-  },
-  migrations: {
-    tableName: 'knex_migrations'
+    pool: {
+      min: 2,
+      max: 10
+    },
+    migrations: {
+      tableName: 'knex_migrations'
+    }
   }
-}
