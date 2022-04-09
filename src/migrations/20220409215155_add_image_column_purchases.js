cb69d552a83b3ed25b4510eaabe367c73069eb49
andrerrh <andre.rodrigues.he@gmail.com>
2022-04-09 18:59:29 -0300

    a

---
+++ src/migrations/20220409215155_add_image_column_purchases.js
@@ -0,0 +1,18 @@
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+ exports.up = function(knex) {
+    return knex.schema.alterTable('purchases', table => {
+    })
+};
+
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.down = function(knex) {
+    return knex.schema.alterTable('purchases', table => {
+        table.dropColumn('image')
+    })
+};
